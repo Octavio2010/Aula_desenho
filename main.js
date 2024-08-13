bba1a1a5aa043cc457c361390ff6bcd16176d09a
Octavio2010 <131196314+Octavio2010@users.noreply.github.com>
2024-08-13 15:34:33 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 function setup(){
     canvas=createCanvas(400,400)
     canvas.center()
+    background("white")
 }
 function draw(){
+    strokeWeight(12);
+    stroke(0);
+    if(mouseIsPressed){
+        line(pmouseX, pmouseY, mouseX, mouseY)
+    }
+}
+
+function limpar(){
     background("white")
 }
