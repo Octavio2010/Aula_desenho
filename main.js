a5e440d0aa5065fa9766f5d893250719034fcbcc
Octavio2010 <131196314+Octavio2010@users.noreply.github.com>
2024-08-26 17:04:28 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
+var classifier = ""
+
+function preload(){
+    classifier = ml5.imageClassifier('DoodleNet')
+}
+
 function setup(){
     canvas=createCanvas(400,400)
     canvas.center()
     background("white")
+    canvas.mouseReleased(classifyCanvas)
+    synth = window.speechSynthesis
 }
 function draw(){
     strokeWeight(12);
@@ -9,6 +17,20 @@ function draw(){
     if(mouseIsPressed){
         line(pmouseX, pmouseY, mouseX, mouseY)
     }
+}
+function classifyCanvas(){
+    classifier.classify(canvas, gotResult)
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error)
+    }
+ else{
+    console.log(results)
+    var result = results[0].label
+    document.getElementById("nome_esboco").innerHTML = "nome: " + result.replace("_", " ")
+ }
 }
 
 function limpar(){
